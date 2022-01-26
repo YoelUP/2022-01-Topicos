a588b5bc87bfb79bf0ae3ad6fb6635579d3ff4f7
YoelUP <78626866+YoelUP@users.noreply.github.com>
2022-01-26 14:09:05 -0600

    Creacion de Logica de Dado

---
+++ Topicos.Codigo/Topicos.Codigo.Dado.BL/Dado.cs
@@ -0,0 +1,48 @@
+﻿namespace Topicos.Codigo.Dado.BL
+{
+    /// <summary>
+    /// Clase Business Logic de Dado
+    /// </summary>
+    public class Dado
+    {
+        const int _NoInicializado = -1;
+        private int _valor;
+
+        public int Valor
+        {
+            get { return _valor; }
+            set { _valor = value; }
+        }
+
+        public Dado()
+        {
+            Valor = _NoInicializado;
+        }
+
+        public Dado(int valor)
+        {
+            Valor = valor;
+        }
+
+        /// <summary>
+        /// Metodo Tirar sin Parametros
+        /// </summary>
+        public void Tirar()
+        {
+            var random = new Random();
+            int elNumero = random.Next(1, 6);
+            Valor = elNumero;
+        }
+
+        /// <summary>
+        /// Metodo Tirar con Parametros
+        /// </summary>
+        public void Tirar(int cantidadVeces)
+        {
+            for (int i = 0; i < cantidadVeces; i++)
+            {
+                this.Tirar();
+            }
+        }
+    }
+}
